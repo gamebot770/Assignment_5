55cfd3df7356b037fd83532dcaf4ba817fd200d6
brandonstanley <brandonspstanley@gmail.com>
2021-11-19 00:15:48 -0600

    Updated with Task 2 Complete

--- main.py
+++ main.py
@@ -49,8 +49,12 @@ def isCourtDoc(docID):
         return 1
     return 0
 
+def make_np_array_with_one_set_val(index, value, length_of_array):
+    arr = np.zeros(length_of_array)
+    arr[index] = value
+    return arr
 
-corpus = sc.textFile("SmallTrainingDataOneLinePerDoc.txt")
+corpus = sc.textFile("s3://chrisjermainebucket/comp330_A5/SmallTrainingDataOneLinePerDoc.txt")
 
 # each entry in validLines will be a line from the text file
 validLines = corpus.filter(lambda x : 'id' in x)
@@ -120,40 +124,43 @@ normalized_TF_IDF = normalized_TF_IDF.zipWithIndex()
 n = normalized_TF_IDF.count()
 
 #TODO: REVIEW THIS
-r = np.ones(20000) * 0.01
-l = 0.1
+# r = np.ones(20000) * 0.01
+# l = 0.1
 x = normalized_TF_IDF
-labels = normalized_TF_IDF.map(lambda x: isCourtDoc(x[0][0])).fold(np.array([]), lambda acc, ele: np.append(acc, ele))
+x.cache()
+labels = x.map(lambda x: (x[1], isCourtDoc(x[0][0]))).map(lambda x: make_np_array_with_one_set_val(x[0], x[1], 20000)).reduce(lambda acc, ele: np.add(acc, ele))
 y = labels
+print(x.filter(lambda x: isCourtDoc(x[0][0]) != y[x[1]]).collect())
+
+# theta = x.map(lambda x: (x[0][0], np.dot(x[0][1], r))).fold(('', np.array([])), lambda acc, ele: (acc[0], np.append(acc[1], ele[1])))[1]
+# e_to_the_theta = np.exp(theta)
+# one_plus_e_to_the_theta = 1 + e_to_the_theta
+# log_one_plus_e_to_the_theta = np.log(1 + e_to_the_theta)
+# error = np.linalg.norm(r, ord=2) * l
+# negative_y_theta = y * -1 * theta
+# total_vector = negative_y_theta + log_one_plus_e_to_the_theta + error
+# total = np.sum(total_vector)
+#
+# #gradient
+# negative_y_x = x.map(lambda x: ((x[0][0], x[0][1] * y[x[1]] * -1), x[1]))
+# J = np.divide(e_to_the_theta, one_plus_e_to_the_theta)
+#
+# P1 = x.map(lambda x: ((x[0][0], x[0][1] * y[x[1]] * -1)))
+# P2 = x.map(lambda x: ((x[0][0], x[0][1] * J[x[1]])))
+# P3 = x.map(lambda x: ((x[0][0], l * r[x[1]])))
+#
+# gradient_raw = P1.join(P2).join(P3).map(lambda x: (x[0], x[1][0][0] + x[1][0][1] + x[1][1]))
+# new_gradient = gradient_raw.fold(('', np.zeros(20000)), lambda acc, ele: (acc[0], acc[1] + ele[1]))[1]
 
 
-theta = x.map(lambda x: (x[0][0], np.dot(x[0][1], r))).fold(('', np.array([])), lambda acc, ele: (acc[0], np.append(acc[1], ele[1])))[1]
-e_to_the_theta = np.exp(theta)
-one_plus_e_to_the_theta = 1 + e_to_the_theta
-log_one_plus_e_to_the_theta = np.log(1 + e_to_the_theta)
-error = np.linalg.norm(r, ord=2) * l
-negative_y_theta = y * -1 * theta
-total_vector = negative_y_theta + log_one_plus_e_to_the_theta + error
-total = np.sum(total_vector)
-
-#gradient
-negative_y_x = x.map(lambda x: ((x[0][0], x[0][1] * y[x[1]] * -1), x[1]))
-J = np.divide(e_to_the_theta, one_plus_e_to_the_theta)
-
-P1 = x.map(lambda x: ((x[0][0], x[0][1] * y[x[1]] * -1)))
-P2 = x.map(lambda x: ((x[0][0], x[0][1] * J[x[1]])))
-P3 = x.map(lambda x: ((x[0][0], l * r[x[1]])))
-
-gradient_raw = P1.join(P2).join(P3).map(lambda x: (x[0], x[1][0][0] + x[1][0][1] + x[1][1]))
-new_gradient = gradient_raw.fold(('', np.zeros(20000)), lambda acc, ele: (acc[0], acc[1] + ele[1]))[1]
 
 def f(x, y, r, l):
-    theta = x.map(lambda x: (x[0][0], np.dot(x[0][1], r))).fold(('', np.array([])),
-                                                                lambda acc, ele: (acc[0], np.append(acc[1], ele[1])))[1]
+    size_of_x = x.count()
+    theta = x.map(lambda x: (x[1], np.dot(x[0][1], r))).map(lambda x_ele: make_np_array_with_one_set_val(x_ele[0], x_ele[1], size_of_x)).reduce(lambda acc, ele: np.add(acc, ele))
     e_to_the_theta = np.exp(theta)
     one_plus_e_to_the_theta = 1 + e_to_the_theta
-    log_one_plus_e_to_the_theta = np.log(1 + e_to_the_theta)
-    error = np.linalg.norm(r, ord=2) * l
+    log_one_plus_e_to_the_theta = np.log(one_plus_e_to_the_theta)
+    error = np.square(np.linalg.norm(r, ord=2)) * l
     negative_y_theta = y * -1 * theta
     total_vector = negative_y_theta + log_one_plus_e_to_the_theta + error
     total = np.sum(total_vector)
@@ -161,11 +168,15 @@ def f(x, y, r, l):
 
 
 def gradient(x, y, r, l):
-    negative_y_x = x.map(lambda x: ((x[0][0], x[0][1] * y[x[1]] * -1), x[1]))
+    size_of_x = x.count()
+    theta = x.map(lambda x: (x[1], np.dot(x[0][1], r))).map(
+        lambda x: make_np_array_with_one_set_val(x[0], x[1], size_of_x)).reduce(lambda acc, ele: np.add(acc, ele))
+    e_to_the_theta = np.exp(theta)
+    one_plus_e_to_the_theta = 1 + e_to_the_theta
     J = np.divide(e_to_the_theta, one_plus_e_to_the_theta)
     P1 = x.map(lambda x: ((x[0][0], x[0][1] * y[x[1]] * -1)))
     P2 = x.map(lambda x: ((x[0][0], x[0][1] * J[x[1]])))
-    P3 = x.map(lambda x: ((x[0][0], l * r[x[1]])))
+    P3 = x.map(lambda x: ((x[0][0], 2 * l * r[x[1]])))
     gradient_raw = P1.join(P2).join(P3).map(lambda x: (x[0], x[1][0][0] + x[1][0][1] + x[1][1]))
     new_gradient = gradient_raw.fold(('', np.zeros(20000)), lambda acc, ele: (acc[0], acc[1] + ele[1]))[1]
     return new_gradient
@@ -175,55 +186,30 @@ def gd_optimize (x, y, w, c):
     rate = 1
     w_last = w + np.full (20000, 1.0)
     while (abs(f(x, y, w, c) - f(x, y, w_last, c)) > 10e-4):
-         w_last = w
-         # print(gradient(x, y, w, c))
-         w = w - rate * gradient (x, y, w, c)
-         if f (x, y, w, c) > f (x, y, w_last, c):
-              rate = rate * .5
-         else:
-              rate = rate * 1.1
-         print (f (x, y, w, c))
+        w_last = w
+        print(gradient(x, y, w, c))
+        w = w - rate * gradient (x, y, w, c)
+        if f (x, y, w, c) > f (x, y, w_last, c):
+            rate = rate * .5
+        else:
+            rate = rate * 1.1
+        print (f (x, y, w, c))
+        results = training_data.map(lambda x: (np.dot(x[0][1], w) > 0.001)).map(lambda x: (int(x))).fold(np.array([]), lambda acc, ele: np.append(acc, ele))
+        print(results.size)
+        total_correct_guesses = np.logical_and(results, y_training).sum()
+        total_errors = np.logical_xor(results, y_training).sum()
+        print("CORRECT: total_correct_guesses ", total_correct_guesses, " errors", total_errors)
     return w
 
 
 
-def predictLabel(k, inputString):
-    words = sc.parallelize(inputString.split(" "))
-    wordsPairs = words.map(lambda x: (x, 0))
-    wordsToIndices= wordsPairs.join(dictionary).map(lambda x: (x[0], x[1][1]))
-    groupedWordsForNumpyIndexing = wordsToIndices.map(lambda x: (x[1], 1)).reduceByKey(lambda a, b: a + b).map(lambda x: ("group", x)).groupBy(lambda x: x[0]).mapValues(list)
-    wordCounts = groupedWordsForNumpyIndexing.map(lambda x: keyPairsToNumpyArray(x[1]))
-    docSize = wordCounts.map(lambda x: x.sum()).take(1)[0]
-    TF_INPUT = wordCounts.map(lambda x: devvec(x, docSize)).take(1)[0]
-    TF_IDF_INPUT = TF_INPUT * IDF
-    distances = TF_IDF.map(lambda x: (x[0], mse_alter(x[1], TF_IDF_INPUT))).cache()
-    top_documents = distances.map(lambda x: (x[0].split("/")[1], 1)).takeOrdered(10, lambda x: x[1])
-    documentSums = sc.parallelize(distances.map(lambda x: (x[0].split("/")[1], (x[1], 1))).takeOrdered(10, lambda x: x[1]))
-    finalOptions = documentSums.reduceByKey(lambda a, b: (min(a[0], b[0]), a[1] + b[1])).take(10)
-    finalResults = defaultdict(list)
-    top_count = -float('inf')
-    for item in finalOptions:
-        count = item[1][1]
-        doc = item[0]
-        score = item[1][0]
-        top_count = max(top_count, count)
-        finalResults[count].append((score, doc))
-    final_candidates = finalResults[top_count]
-    print(min(final_candidates, key=lambda x: x[0])[1])
-    print(finalOptions)
 
-
-
-    # a = TF_IDF.lookup("20_newsgroups/comp.graphics/37261")[0]
-    # b = TF_IDF.lookup("20_newsgroups/talk.politics.mideast/75944")[0]
-    # c = TF_IDF.lookup("20_newsgroups/sci.med/58763")[0]
-
-predictLabel(3, "Where do the gods rest upon the shoulders of man. How can we deny his existence?")
+# predictLabel(3, "Where do the gods rest upon the shoulders of man. How can we deny his existence?")
 
 number_of_training = 2000
 y_training = y[:number_of_training]
 y_testing = y[number_of_training:]
-modified_x_data =  x.map(lambda x: ((x[0][0], x[0][1] * 0.01), x[1]))
+modified_x_data =  x.map(lambda x: ((x[0][0], x[0][1] * 0.0001), x[1]))
 training_data = modified_x_data.filter(lambda x: x[1] < number_of_training)
 testing_data = modified_x_data.filter(lambda x: x[1] >= number_of_training)
 
@@ -231,9 +217,9 @@ training_data.cache()
 testing_data.cache()
 modified_x_data.cache()
 
-optimized = gd_optimize(training_data, y_training, r, 0.1)
+optimized = gd_optimize(training_data, y_training, np.random.normal(0, 0.0001, 20000), 0.005)
 
-results = testing_data.map(lambda x: (np.dot(x[0][1], optimized) > 0)).map(lambda x: (int(x))).fold(np.array([]), lambda acc, ele: np.append(acc, ele))
+results = testing_data.map(lambda x: (np.dot(x[0][1], optimized) > 0.001)).map(lambda x: (int(x))).fold(np.array([]), lambda acc, ele: np.append(acc, ele))
 
 total_true = y.sum()
 total_correct_guesses = np.logical_and(results, y_testing).sum()
@@ -241,3 +227,11 @@ total_errors = np.logical_xor(results, y_testing).sum()
 
 print("CORRECT: total_correct_guesses ", total_correct_guesses)
 print("WRONG: ", total_errors)
+
+topFiftyIndices = np.argsort(optimized * -1)[:50]
+topFiftyWordsForAUCases = dictionary.filter(lambda x: x[1] in topFiftyIndices).take(50)
+
+#Top 50 Words
+"""
+[('that', 12), ('not', 28), ('mr', 206), ('applicant', 347), ('decision', 360), ('evidence', 365), ('whether', 430), ('tribunal', 479), ('respondent', 710), ('appeal', 751), ('reasons', 946), ('costs', 987), ('respect', 1077), ('ltd', 1141), ('relation', 1145), ('notice', 1148), ('sought', 1197), ('conduct', 1228), ('relevant', 1243), ('circumstances', 1320), ('honour', 1491), ('proceedings', 1772), ('error', 2227), ('satisfied', 2321), ('judgment', 2322), ('pty', 2327), ('pursuant', 2926), ('submissions', 2996), ('respondents', 3082), ('affidavit', 3122), ('counsel', 3299), ('fca', 3600), ('discretion', 3867), ('hca', 3887), ('clr', 3982), ('relied', 4048), ('fcr', 4273), ('multicultural', 4755), ('delegate', 4991), ('jj', 5172), ('jurisdictional', 6169), ('alr', 6322), ('fairness', 6611), ('interlocutory', 6729), ('relevantly', 7042), ('factual', 7618), ('fcafc', 8083), ('affidavits', 9238), ('gummow', 9669), ('gaudron', 17308)]
+"""
